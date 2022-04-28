fa4119a38c33dafa1e4e1fd044ad7b0d230643a8
MALAK_s2021 <malaksaidi16@gmail.com>
2022-04-28 07:40:28 +0200

    mail config

--- src/main/java/org/sweetrooms/utils/EmailSender.java
+++ src/main/java/org/sweetrooms/utils/EmailSender.java
@@ -7,8 +7,6 @@ import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -16,7 +14,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.context.Context;
-import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -34,7 +32,7 @@ public class EmailSender {
 	private Environment environment;
 
 	public EmailSender() {
-		
+
 	}
 
 	@Async
